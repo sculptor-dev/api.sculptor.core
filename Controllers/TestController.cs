67c86fec3a9f47c781e5ff2b192793e5ed75ae85
Tucker Lein <self@tuckerle.in>
2019-08-18 11:53:57 -0400

    feat: API-3 Initial .NET Core project setup

---
+++ Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.sculptor.core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public class EchoRequest
+        {
+            public string toEcho;
+        }
+
+        // POST api/test/echo
+        [HttpPost]
+        [Route("echo")]
+        public string Echo([FromBody] EchoRequest request)
+        {
+            return request.toEcho;
+        }
+    }
+}
